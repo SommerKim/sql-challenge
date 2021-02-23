0cdc833ddf4c4c7051326e24cd9f2e4d563705a6
SommerKim <74261427+SommerKim@users.noreply.github.com>
2021-02-23 13:12:08 -0500

    Queries 2-4

--- EmployeeSQL/query.sql
+++ EmployeeSQL/query.sql
@@ -4,4 +4,28 @@ FROM employees e
 JOIN salaries s
 ON e.emp_no = s.emp_no;
 
+-- Query for Question 2
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date LIKE '%1986';
 
+-- Query for Question 3
+SELECT m.dept_no, d.dept_name, e.emp_no, e.last_name, e.first_name
+FROM employees e
+JOIN dept_manager m
+ON e.emp_no = m.emp_no
+JOIN departments d
+ON m.dept_no = d.dept_no;
+
+-- Query for Question 4
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees e
+JOIN dept_emp p
+ON e.emp_no = p.emp_no
+JOIN departments d
+ON p.dept_no = d.dept_no;
+
+-- Query for Question 5
+
+
+SELECT * FROM employees;
